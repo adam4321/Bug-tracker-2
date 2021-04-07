cf78bb28c30b476719e73ad5ebaed4622e0322e3
Adam Wright <adam@adamjwright.com>
2021-04-07 01:36:03 -0700

    401 route working

---
+++ src/main/java/com/adamjwright/bug_tracker/controllers/UnauthorizedPage.java
@@ -0,0 +1,34 @@
+package com.adamjwright.bug_tracker.controllers;
+
+import java.io.IOException;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UnauthorizedPage {
+
+    // The login page which is the first page rendered when visiting the app
+    @RequestMapping("/unauthorized")
+	public String index() throws IOException {
+        // Set the directory and file extension of the templates
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".hbs");
+
+        // Select the outer layout and inner body templates
+        Handlebars handlebars = new Handlebars(loader);
+        Template layout = handlebars.compile("layouts/login");
+        Template body = handlebars.compile("unauthorized-page");
+
+        // Parse into a string and return
+        String bodyStr = body.apply("");
+        String templateString = layout.apply(bodyStr);
+		return templateString;
+	}
+}
