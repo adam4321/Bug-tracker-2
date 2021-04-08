70351c0c86fc9c8bc05b25da6ffbfd6558765c99
Adam Wright <adam@adamjwright.com>
2021-04-08 13:34:46 -0700

    Displaying user pic and email

--- src/main/java/com/adamjwright/bug_tracker/controllers/YourBugs.java
+++ src/main/java/com/adamjwright/bug_tracker/controllers/YourBugs.java
@@ -1,6 +1,12 @@
+/******************************************************************************
+**  /home route displays the your-bugs page which is the 1st the user sees
+******************************************************************************/
+
 package com.adamjwright.bug_tracker.controllers;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.adamjwright.bug_tracker.HandlebarsHelpers;
 import com.github.jknack.handlebars.Handlebars;
@@ -8,15 +14,24 @@ import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class YourBugs {
 
     // "Your bugs" the initial dashboard page -- render the bug list
-    @RequestMapping("/home")
-	public String index() throws IOException {
+    @GetMapping("/home")
+	public String index(Authentication authentication) throws IOException {
+        // Retrieve the user data from the oauth token
+        OAuth2AuthenticatedPrincipal principal = (OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
+        Map<String, Object> context = new HashMap<>();
+        context.put("user", principal.getAttributes());
+
+        // System.out.println("\n-- REQUEST --\n" + context.get("user") + "\n-- REQUEST --\n");
+
         // Set the directory and file extension of the templates
         TemplateLoader loader = new ClassPathTemplateLoader();
         loader.setPrefix("/templates");
@@ -31,8 +46,9 @@ public class YourBugs {
         Template body = handlebars.compile("your-bugs");
 
         // Parse into a string and return
-        String bodyStr = body.apply("");
-        String templateString = layout.apply(bodyStr);
+        Object bodyStr = body.apply("");
+        context.put("body", bodyStr);
+        String templateString = layout.apply(context);
 		return templateString;
 	}
 }
