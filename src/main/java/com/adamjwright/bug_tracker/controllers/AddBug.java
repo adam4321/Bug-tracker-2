fd83060bde5152e0dc119d3c25490d2bac5a7594
Adam Wright <adam@adamjwright.com>
2021-04-09 13:40:28 -0700

    Add get route

---
+++ src/main/java/com/adamjwright/bug_tracker/controllers/AddBug.java
@@ -0,0 +1,47 @@
+package com.adamjwright.bug_tracker.controllers;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.adamjwright.bug_tracker.HandlebarsHelpers;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddBug {
+    
+    @GetMapping("/add_bug")
+    public String index(Authentication authentication) throws IOException {
+        // Retrieve the user data from the oauth token
+        OAuth2AuthenticatedPrincipal principal = (OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
+        Map<String, Object> context = new HashMap<>();
+        context.put("user", principal.getAttributes());
+
+        // Set the directory and file extension of the templates
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".hbs");
+
+        // Create handlebars object and add helper methods
+        Handlebars handlebars = new Handlebars(loader);
+        handlebars.registerHelpers(HandlebarsHelpers.class);
+
+        // Select the outer layout and inner body templates
+        Template layout = handlebars.compile("layouts/main");
+        Template body = handlebars.compile("add-bug");
+
+        // Parse into a string and return
+        Object bodyStr = body.apply("");
+        context.put("body", bodyStr);
+        String templateString = layout.apply(context);
+		return templateString;
+	}
+}
