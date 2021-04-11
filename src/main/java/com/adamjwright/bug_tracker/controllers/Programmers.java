f2fc33c80612192eb55a6859f625516bacde5c3a
Adam Wright <adam@adamjwright.com>
2021-04-11 15:04:17 -0700

    Programmers get route working

---
+++ src/main/java/com/adamjwright/bug_tracker/controllers/Programmers.java
@@ -0,0 +1,102 @@
+/******************************************************************************
+**  /programmers routes
+**
+**  /programmers/ - displays the list of current companies
+******************************************************************************/
+
+package com.adamjwright.bug_tracker.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import com.adamjwright.bug_tracker.HandlebarsHelpers;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Programmers {
+    
+    // Displays the list of current programmers
+    @GetMapping("/programmers")
+    public String index(Authentication authentication) throws IOException {
+        // Retrieve the user data from the oauth token
+        OAuth2AuthenticatedPrincipal principal = (OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
+        Map<String, Object> context = new HashMap<>();
+        context.put("user", principal.getAttributes());
+        
+        // Get database configuration
+        ResourceBundle reader = ResourceBundle.getBundle("dbconfig");
+        String CONNECTION_URL = reader.getString("db.url");
+        String DB_USER = reader.getString("db.username");
+        String DB_PASSWORD = reader.getString("db.password");
+
+        // Define sql query
+        String sqlSelectAllProgrammers = "SELECT * FROM Programmers ORDER BY dateStarted DESC";
+
+        // Array of maps to hold the company data
+        ArrayList<Map<String, String>> programmersDbData = new ArrayList<Map<String, String>>();
+
+        // Connect to db
+        try (Connection conn = DriverManager.getConnection(CONNECTION_URL, DB_USER, DB_PASSWORD); 
+            PreparedStatement ps = conn.prepareStatement(sqlSelectAllProgrammers); 
+            ResultSet rs = ps.executeQuery()) {
+
+                // Iterate over result set
+                while (rs.next()) {
+                    Map<String, String> hm = new HashMap<>();
+
+                    hm.put("programmerId", rs.getString("programmerId"));
+                    hm.put("lastName", rs.getString("firstName"));
+                    hm.put("firstName", rs.getString("lastName"));
+                    hm.put("email", rs.getString("email"));
+                    hm.put("dateStarted", rs.getString("dateStarted"));
+                    hm.put("accessLevel", rs.getString("accessLevel"));
+
+                    programmersDbData.add(hm);
+                }
+
+            conn.close();
+        } 
+        catch (SQLException e) {
+            // Handle a failed db connection
+            e.printStackTrace();
+        }
+
+        // Add the company data to the context object
+        context.put("programmers", programmersDbData);
+
+        // Set the directory and file extension of the templates
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".hbs");
+
+        // Create handlebars object and add helper methods
+        Handlebars handlebars = new Handlebars(loader);
+        handlebars.registerHelpers(HandlebarsHelpers.class);
+
+        // Select the outer layout and inner body templates
+        Template layout = handlebars.compile("layouts/main");
+        Template body = handlebars.compile("programmers");
+
+        // Parse into a string and return
+        Object bodyStr = body.apply(context);
+        context.put("body", bodyStr);
+        String templateString = layout.apply(context);
+        return templateString;
+    }
+}
