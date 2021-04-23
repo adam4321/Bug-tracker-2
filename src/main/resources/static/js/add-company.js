ffb33c5d276f1028ef4d3b7ab14fd218d6c80422
Adam Wright <adam@adamjwright.com>
2021-04-23 16:37:27 -0700

    Add csrd cookie remove reset

--- src/main/resources/static/js/add-company.js
+++ src/main/resources/static/js/add-company.js
@@ -14,7 +14,7 @@ recordForm.addEventListener('submit', (e) => {
     e.preventDefault();
     spinner.style.visibility = "visible"; 
     let req = new XMLHttpRequest();
-    let path = '/bug_tracker/companies/add_company/insertCompany';
+    let path = '/bug_tracker/add_company/insertCompany';
 
     // String that holds the form data
     let reqBody = {
@@ -27,10 +27,10 @@ recordForm.addEventListener('submit', (e) => {
     // Ajax request
     req.open('POST', path, true);
     req.setRequestHeader('Content-Type', 'application/json');
+    req.setRequestHeader("X-XSRF-TOKEN", Cookies.get('XSRF-TOKEN'));
     req.addEventListener('load', () => {
         if (req.status >= 200 && req.status < 400) {
             // Clear the submit form and stop spinner
-            document.getElementById('recordForm').reset();
             setTimeout(() => { spinner.style.visibility = "hidden"; }, 1000);
             
             // Redirect to companies page
