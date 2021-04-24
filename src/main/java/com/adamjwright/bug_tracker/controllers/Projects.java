9cdabcba397f8e7ce2f17f298a63c5774fdd430d
Adam Wright <adam@adamjwright.com>
2021-04-24 01:18:34 -0700

    Delete projects working

--- src/main/java/com/adamjwright/bug_tracker/controllers/Projects.java
+++ src/main/java/com/adamjwright/bug_tracker/controllers/Projects.java
@@ -1,7 +1,7 @@
 /******************************************************************************
 **  /projects routes
 **
-**  /projects/ - displays the list of current projects
+**  / -> displays the list of current projects
 ******************************************************************************/
 
 package com.adamjwright.bug_tracker.controllers;
@@ -12,6 +12,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -28,6 +29,8 @@ import com.github.jknack.handlebars.io.TemplateLoader;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,7 +38,7 @@ public class Projects {
 
     // Displays the list of current projects
     @GetMapping("/projects")
-    public String index(Authentication authentication, HttpServletRequest request) throws IOException {
+    public String renderProjects(Authentication authentication, HttpServletRequest request) throws IOException {
         // Retrieve the user data from the oauth token
         OAuth2AuthenticatedPrincipal principal = (OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
         Map<String, Object> context = new HashMap<>();
@@ -58,32 +61,40 @@ public class Projects {
         String DB_PASSWORD = reader.getString("db.password");
 
         // Define sql query
-        String sqlSelectAllCompanies = "SELECT * FROM Companies ORDER BY dateJoined DESC";
+        String sqlProjectsQuery = "SELECT * FROM Projects AS p JOIN Companies AS c ON p.companyId = c.companyId"
+                               + " ORDER BY dateStarted DESC, projectId DESC";
+
+        // Array of maps to hold the company data
+        ArrayList<Map<String, String>> projectDbData = new ArrayList<Map<String, String>>();
 
         // Connect to db
         try (Connection conn = DriverManager.getConnection(CONNECTION_URL, DB_USER, DB_PASSWORD); 
-            PreparedStatement ps = conn.prepareStatement(sqlSelectAllCompanies); 
+            PreparedStatement ps = conn.prepareStatement(sqlProjectsQuery); 
             ResultSet rs = ps.executeQuery()) {
 
                 // Iterate over result set
                 while (rs.next()) {
-                    String id = rs.getString("companyId");
-                    String name = rs.getString("companyName");
-                    String dateJoined = rs.getString("dateJoined");
+                    Map<String, String> hm = new HashMap<>();
 
-                    System.out.println("-- " + id + " " + name + " " + dateJoined);
+                    hm.put("projectId", rs.getString("projectId"));
+                    hm.put("projectName", rs.getString("projectName"));
+                    hm.put("companyName", rs.getString("companyName"));
+                    hm.put("dateStarted", rs.getString("dateStarted"));
+                    hm.put("lastUpdated", rs.getString("lastUpdated"));
+                    hm.put("inMaintenance", rs.getString("inMaintenance"));
                     
+                    projectDbData.add(hm);
                 }
 
             conn.close();
         } 
+        // Handle a failed db connection
         catch (SQLException e) {
-            // Handle a failed db connection
             e.printStackTrace();
         }
 
-
-        // System.out.println("\n-- CONTEXT OBJ --\n" + context.get("user") + "\n-- CONTEXT OBJ --\n");
+        // Add the company data to the context object
+        context.put("projects", projectDbData);
 
         // Set the directory and file extension of the templates
         TemplateLoader loader = new ClassPathTemplateLoader();
@@ -103,5 +114,35 @@ public class Projects {
         context.put("body", bodyStr);
         String templateString = layout.apply(context);
         return templateString;
+    }
+
+
+    // Delete a project from the list
+    @PostMapping("/projects/deleteProject")
+    public void deleteProject(@RequestBody Map<String, Object> payload) throws IOException {
+
+        // Get database configuration
+        ResourceBundle reader = ResourceBundle.getBundle("dbconfig");
+        String CONNECTION_URL = reader.getString("db.url");
+        String DB_USER = reader.getString("db.username");
+        String DB_PASSWORD = reader.getString("db.password");
+
+        // Define sql query
+        String sqlDeleteCompany = "DELETE FROM Projects WHERE projectId = ?";
+
+        // Connect to db
+        try (Connection conn = DriverManager.getConnection(CONNECTION_URL, DB_USER, DB_PASSWORD); 
+            PreparedStatement ps = conn.prepareStatement(sqlDeleteCompany)) {
+
+            // Set the query string param companyId in the mysql prepared statement
+            ps.setInt(1, (Integer)payload.get("projectId"));
+            ps.executeUpdate();
+
+            conn.close();
+        } 
+        // Handle a failed db connection
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
